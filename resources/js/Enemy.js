bd5b9f510139ebbfb5577d927e9a6058ff425a73
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-07-17 09:18:50 -0400

    move enemy towards player

--- resources/js/Enemy.js
+++ resources/js/Enemy.js
@@ -8,11 +8,24 @@ class Enemy extends Phaser.GameObjects.Sprite {
     }
 
     update() {
+        // todo: detect if enemy has line of sight to player
+        // var ray = new Phaser.Geom.Line(this.x, this.y, this.scene.player.x, this.scene.player.y);
+        // var walls = this.scene.wallsLayer.getTilesWithinShape(ray); - this doesn't work
+        // if (walls.length > 0) return;
+
         // calculate rotation to face player
         var dx = this.scene.player.x - this.x;
         var dy = this.scene.player.y - this.y;
         var angle = Math.atan2(dy, dx);
         this.rotation = angle + Phaser.Math.DegToRad(90);
+        
+        // move enemy towards player while enemy is 100 units away
+        if (Phaser.Math.Distance.Between(this.x, this.y, this.scene.player.x, this.scene.player.y) > 100) {
+            this.body.velocity.x = Math.cos(angle) * 50;
+            this.body.velocity.y = Math.sin(angle) * 50;
+        }else{
+            this.body.velocity.set(0);
+        }
 
         const offset = new Phaser.Math.Vector2(0, -40);
         Phaser.Math.Rotate(offset, this.rotation);
