3f5e0270cf5b90302bd47f1780ff8b9c2a81d235
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-07-19 09:52:57 -0400

    grouped effects classes into one file

--- resources/js/Explosion.js
+++
@@ -1,9 +0,0 @@
-class Explosion extends Phaser.GameObjects.Sprite {
-  constructor(scene, x, y) {
-    super(scene, x, y, "explosion");
-    scene.add.existing(this);
-    this.scale = settings.scale * 2;
-    scene.sound.add("explosionSound").play();
-    this.play("explosionAnimation");
-  }
-}
--- resources/js/Flash.js
+++
@@ -1,9 +0,0 @@
-class Flash extends Phaser.GameObjects.Sprite {
-  constructor(scene, x, y, rotation) {
-    super(scene, x, y, "flash");
-    scene.add.existing(this);
-    this.scale = settings.scale * 2;
-    this.rotation = rotation;
-    this.play("flashAnimation");
-  }
-}
